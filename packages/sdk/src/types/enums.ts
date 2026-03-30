a9b7f635df16783a896c42a7790e115a0c5083ed
ghostyubin <yubinqqyx@qq.com>
2026-03-31 03:11:18 +0800

    再次修复编译错误

--- packages/sdk/src/types/enums.ts
+++ packages/sdk/src/types/enums.ts
@@ -1,31 +1,31 @@
-export const enum EUploadRtype {
+export enum EUploadRtype {
   FAIL = 0,
   RENAME = 1,
   DIFF_RENAME = 2,
   OVERWRITE = 3,
 }
 
-export const enum EFileManageOndup {
+export enum EFileManageOndup {
   FAIL = 'fail',
   OVERWRITE = 'overwrite',
   NEWCOPY = 'newcopy',
   SKIP = 'skip',
 }
 
-export const enum EFileManageAsync {
+export enum EFileManageAsync {
   SYNC = 0,
   ADAPTIVE = 1,
   ASYNC = 2,
 }
 
-export const enum EStepStatus {
+export enum EStepStatus {
   CREATED = 0,
   RUNNING = 1,
   STOPPED = 2,
   FINISHED = 3,
 }
 
-export const enum EUploadSteps {
+export enum EUploadSteps {
   FILE_INFO = 1,
   LOCAL_MD5 = 2,
   UPLOAD_ID = 3,
@@ -38,7 +38,7 @@ export const enum EUploadSteps {
   FINISH = 10,
 }
 
-export const enum EDownloadSteps {
+export enum EDownloadSteps {
   FSID_BY_PATH = 1,
   DLINK_BY_FSID = 2,
   DOWNLOAD_INFO = 3,
